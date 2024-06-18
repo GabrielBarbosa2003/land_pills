438390048520a5242366230a1a8e0a4a6129c03e
gabriel <gabrieldecarvalhu@gmail.com>
2024-06-18 14:43:22 -0300

    mais responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import load from './assets/img/load.png'
 import { gsap } from "gsap";
 import { useEffect, useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react';
-import  Menu from './components/Menu/index.jsx'
+import Menu from './components/Menu/index.jsx'
 
 
 
@@ -133,7 +133,7 @@ function App() {
         });
 
         gsap.to(".hero-copy h1 span", {
-          top: "120",
+          top: "90",
           stagger: 0.1,
           ease: "power3.inOut",
           duration: 2,
@@ -155,34 +155,34 @@ function App() {
 
 
 
-let tela = window.innerWidth;
+  let tela = window.innerWidth;
   return (
     <>
       <section className='home'>
-        { tela <= 480 ? 
-        <div className='menuu'>
-          <Menu/>
+        {tela <= 480 ?
+          <div className='menuu'>
+            <Menu />
 
-        </div>
-        
-        :
-        <nav className='navbar'>
-        <div className='contact'>
-          <a href='#'>Contact</a>
-          <a href='#'>Contact</a>
-        </div>
+          </div>
 
-        <div className='logo'>
-          <a href='#'>PILLS</a>
-        </div>
+          :
+          <nav className='navbar'>
+            <div className='contact'>
+              <a href='#'>Contact</a>
+              <a href='#'>Contact</a>
+            </div>
 
-        <div className='services'>
-          <a href='#'>Services</a>
-          <a href='#'>Contact</a>
-        </div>
-      </nav>  
-      }
-       
+            <div className='logo'>
+              <a href='#'>PILLS</a>
+            </div>
+
+            <div className='services'>
+              <a href='#'>Services</a>
+              <a href='#'>Contact</a>
+            </div>
+          </nav>
+        }
+
 
 
         <div className='hero'>
@@ -217,7 +217,7 @@ let tela = window.innerWidth;
               <a href='#'><button>Learn more</button></a>
             </div>
           </div>
-        </div> 
+        </div>
         <div className="hero-copy">
           <h1>PILLS</h1>
         </div>
